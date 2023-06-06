9e490734d09015d59cbfa6deaecccc92763499d1
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2023-06-06 03:20:42 -0400

    Added stylesheets folder for styling workflow

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './store';
-import './index.css';
+import store from './app/store';
+import './stylesheets/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,7 +16,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
